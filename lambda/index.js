4734c98400b57f92ffb9d15640ba11663baaca64
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-12-03 22:19:55 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -29,7 +29,7 @@ const HelloWorldIntentHandler = {
         
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            .reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .addDelegateDirective
             .getResponse();
     }
 };
